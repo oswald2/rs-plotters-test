9ba9d533ed3d8b0ed49975500b56896e5948b893
Michael Oswald <michael.oswald@onikudaki.net>
2023-06-04 21:12:52 +0200

    Tried the 3D plot

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use gtk::prelude::*;
 use gtk::{ApplicationWindow, Builder};
 use plotters::prelude::{
     ChartBuilder, Circle, DiscreteRanged, EmptyElement, IntoDrawingArea, IntoLinspace, PathElement,
-    Text,
+    Text, Rectangle,
 };
-use plotters::series::{LineSeries, PointSeries};
-use plotters::style::{full_palette::*, ShapeStyle};
+use plotters::series::{LineSeries, PointSeries, SurfaceSeries};
+use plotters::style::{full_palette::*, ShapeStyle, Color};
 
 
 pub mod backend;
@@ -38,94 +38,62 @@ fn build_ui(application: &gtk::Application) {
     drawing_area.connect_draw(|da, ctx| {
         let width = da.allocated_width();
         let height = da.allocated_height();
-        let root_area = CairoBackend::new(&ctx, (width as u32, height as u32))
+        let area = CairoBackend::new(&ctx, (width as u32, height as u32))
             .unwrap()
             .into_drawing_area();
 
-        root_area.fill(&WHITE).unwrap();
+        area.fill(&WHITE).unwrap();
 
-        let root_area = root_area.titled("Image Title", ("sans-serif", 60)).unwrap();
+        let x_axis = (-3.0..3.0).step(0.1);
+        let z_axis = (-3.0..3.0).step(0.1);
+    
+        let mut chart = ChartBuilder::on(&area)
+            .caption(format!("3D Plot Test"), ("sans", 20))
+            .build_cartesian_3d(x_axis.clone(), -3.0..3.0, z_axis.clone()).unwrap();
+    
+        chart.with_projection(|mut pb| {
+            pb.yaw = 0.5;
+            pb.scale = 0.9;
+            pb.into_matrix()
+        });
+    
+        chart
+            .configure_axes()
+            .light_grid_style(BLACK.mix(0.15))
+            .max_light_lines(3)
+            .draw().unwrap();
+    
+        chart
+            .draw_series(
+                SurfaceSeries::xoz(
+                    (-30..30).map(|f| f as f64 / 10.0),
+                    (-30..30).map(|f| f as f64 / 10.0),
+                    |x, z| (x * x + z * z).cos(),
+                )
+                .style(BLUE.mix(0.2).filled()),
+            ).unwrap()
+            .label("Surface")
+            .legend(|(x, y)| Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled()));
+    
+        chart
+            .draw_series(LineSeries::new(
+                (-100..100)
+                    .map(|y| y as f64 / 40.0)
+                    .map(|y| ((y * 10.0).sin(), y, (y * 10.0).cos())),
+                &BLACK,
+            )).unwrap()
+            .label("Line")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
+    
+        chart
+            .configure_series_labels()
+            .border_style(&BLACK)
+            .draw().unwrap();
+    
+        // To avoid the IO failure being ignored silently, we manually call the present function
+        area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
 
-        let (upper, lower) = root_area.split_vertically(512);
-
-        let x_axis = (-3.4f32..3.4).step(0.1);
-
-        let mut cc = ChartBuilder::on(&upper)
-            .margin(5)
-            .set_all_label_area_size(50)
-            .caption("Sine and Cosine", ("sans-serif", 40))
-            .build_cartesian_2d(-3.4f32..3.4, -1.2f32..1.2f32)
-            .unwrap();
-
-        cc.configure_mesh()
-            .x_labels(20)
-            .y_labels(10)
-            .disable_mesh()
-            .x_label_formatter(&|v| format!("{:.1}", v))
-            .y_label_formatter(&|v| format!("{:.1}", v))
-            .draw()
-            .unwrap();
-
-        cc.draw_series(LineSeries::new(x_axis.values().map(|x| (x, x.sin())), &RED))
-            .unwrap()
-            .label("Sine")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-        cc.draw_series(LineSeries::new(
-            x_axis.values().map(|x| (x, x.cos())),
-            &BLUE,
-        ))
-        .unwrap()
-        .label("Cosine")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
-
-        cc.configure_series_labels().border_style(&BLACK).draw().unwrap();
-
-        /*
-        // It's possible to use a existing pointing element
-         cc.draw_series(PointSeries::<_, _, Circle<_>>::new(
-            (-3.0f32..2.1f32).step(1.0).values().map(|x| (x, x.sin())),
-            5,
-            Into::<ShapeStyle>::into(&RGBColor(255,0,0)).filled(),
-        ))?;*/
-
-        // Otherwise you can use a function to construct your pointing element yourself
-        cc.draw_series(PointSeries::of_element(
-            (-3.0f32..2.1f32).step(1.0).values().map(|x| (x, x.sin())),
-            5,
-            ShapeStyle::from(&RED).filled(),
-            &|coord, size, style| {
-                EmptyElement::at(coord)
-                    + Circle::new((0, 0), size, style)
-                    + Text::new(format!("{:?}", coord), (0, 15), ("sans-serif", 15))
-            },
-        )).unwrap();
-
-        let drawing_areas = lower.split_evenly((1, 2));
-
-        for (drawing_area, idx) in drawing_areas.iter().zip(1..) {
-            let mut cc = ChartBuilder::on(&drawing_area)
-                .x_label_area_size(30)
-                .y_label_area_size(30)
-                .margin_right(20)
-                .caption(format!("y = x^{}", 1 + 2 * idx), ("sans-serif", 40))
-                .build_cartesian_2d(-1f32..1f32, -1f32..1f32).unwrap();
-            cc.configure_mesh()
-                .x_labels(5)
-                .y_labels(3)
-                .max_light_lines(4)
-                .draw().unwrap();
-
-            cc.draw_series(LineSeries::new(
-                (-1f32..1f32)
-                    .step(0.01)
-                    .values()
-                    .map(|x| (x, x.powf(idx as f32 * 2.0 + 1.0))),
-                &BLUE,
-            )).unwrap();
-        } // After this point, we should be able to draw construct a chart context
-
-        root_area.present().unwrap();
+        area.present().unwrap();
 
         Inhibit(true)
     });
